4c530c7759b1fc6595065beb45c0445d698b8622
kedar-1401 <kedardhule14@gmail.com>
2025-03-09 14:51:35 +0530

    Done

---
+++ app.js
@@ -0,0 +1,33 @@
+const express =require('express');
+const app=express();
+const PORT=5000;
+const cors=require('cors')
+
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send({message:"Welcome to CTF"})
+})
+
+app.get('/Auth', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader === '666') {
+      res.json({ message: 'Success', url: 'https://example.com' });
+  } else {
+      res.status(403).json({ error: 'Wrong credentials' });
+  }
+});
+
+app.listen(PORT,() => {
+    console.log(`Example app listening on http://localhost:${PORT}`)
+  })
